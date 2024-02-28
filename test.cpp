39c5f4934066e7f899047c0edb58bebbf7150c4e
nutritions <1601057875@qq.com>
2024-02-28 19:45:46 +0800

    add

--- test.cpp
+++ test.cpp
@@ -1,31 +1,27 @@
 #include <iostream>
-#include <cstring>
+#include <string>
 #include <algorithm>
 using namespace std;
+// const int N=1e5;
+// int next[N];
 
-int n,m;
-int a[500005],s[500005]; //差分的区间和
+// void getNext(string sub){
 
-int lowbit(int x){ //提取x的低位2次幂数
-  return x&-x;
-}
-void change(int x,int k){ //向后修
-  while(x<=n) s[x]+=k, x+=lowbit(x);
-}
-int query(int x){ //向前查
-  int t=0;
-  while(x) t+=s[x], x-=lowbit(x);
-  return t;
-}
+// }
+
+// int KMP(string s,string sub){
+
+// return 0;
+// }
+
 int main(){
-  cin>>n>>m; int op,x,y,k;
-  for(int i=1;i<=n;i++) scanf("%d",&a[i]);
-  for(int i=1;i<=m;i++){
-    scanf("%d%d",&op,&x);
-    if(op==1){
-      scanf("%d%d",&y,&k);
-      change(x,k); change(y+1,-k); //差分
-    }
-    else printf("%d\n",a[x]+query(x));
-  }
+  string s=" ";//主串
+  string sub=" ";//模式串
+  string tmp;
+  cin>>tmp;
+  s.append(tmp);
+  cin>>tmp;
+  sub.append(tmp);
+
+  return 0;
 }
