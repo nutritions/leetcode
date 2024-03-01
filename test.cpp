20496737f10333e104d0d7e5d6d45a7e37b14d2c
nutritions <1601057875@qq.com>
2024-03-01 10:58:50 +0800

    add

--- test.cpp
+++ test.cpp
@@ -3,15 +3,37 @@
 #include <algorithm>
 using namespace std;
 const int N=1e5;
-int next[N];
+int _next[N];
 
 void getNext(string sub){
-
+  _next[1]=0;
+  int i=1,j=0;//i是next索引，j是某个next值
+  while(i<sub.size()-1){
+    if(j==0||sub[i]==sub[j]){
+      _next[++i]=++j;
+    }else{
+      j=_next[j];
+    }
+  }
 }
 
 int KMP(string s,string sub){
-
-return 0;
+  int i=1,j=1;//主串和模式串的指针
+  int slen=s.size()-1;
+  int sublen=sub.size()-1;
+  while(i<=slen&&j<=sublen){
+    if(j==0||s[i]==sub[j]){
+      i++;
+      j++;
+    }else{
+      j=_next[j];
+    }
+  }
+  if(j>sublen){
+    return i-sublen;
+  }else{
+    return 0;
+  }
 }
 
 int main(){
@@ -22,6 +44,14 @@ int main(){
   s.append(tmp);
   cin>>tmp;
   sub.append(tmp);
-
+  int slen=s.size()-1;
+  int sublen=sub.size()-1;
+  getNext(sub);
+  cout<<_next[1]<<endl;
+  cout<<sublen<<endl;
+  for(int i=1;i<=sublen;i++){
+      cout<<_next[i]<<" ";
+  }
+  cout<<KMP(s,sub)<<endl;
   return 0;
 }
